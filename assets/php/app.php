5fe60f6a12f09bdaf7071652bb26acb3be6aa26f
Luciano Franzoi Filho <137215449+Luciano-Franzoi-Filho@users.noreply.github.com>
2024-06-10 22:38:08 -0300

    Update app.php

--- assets/php/app.php
+++ assets/php/app.php
@@ -7,10 +7,12 @@ require_once __DIR__ . '/AlunoModel.php';
 require_once __DIR__ . '/NotaModel.php';
 
 $aluno = new Aluno( ra: $_POST['ra'], 
-                        nome: $_POST['nome'], 
-                        email: $_POST['email']);
+                    nome: $_POST['nome'], 
+                    email: $_POST['email']);
 
-$notas = new Notas( $prova1 = isset($_POST['Prova1']) ? (float)($_POST['Prova1'] ?? 0.0) : 0.0,
+
+
+$notas = new Notas( $aluno, $prova1 = isset($_POST['Prova1']) ? (float)($_POST['Prova1'] ?? 0.0) : 0.0,
 $aep1 = isset($_POST['AEP1']) ? (float)($_POST['AEP1'] ?? 0.0) : 0.0,
 $provaIntegrada1 = isset($_POST['ProvaIntegrada1']) ? (float)($_POST['ProvaIntegrada1'] ?? 0.0) : 0.0,
 $prova2 = isset($_POST['Prova2']) ? (float)($_POST['Prova2'] ?? 0.0) : 0.0,
